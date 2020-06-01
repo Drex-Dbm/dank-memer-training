a1e87d42215b6d32bc2a515b601b6be3861864d5
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-01 15:06:42 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
 
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
-});
+const client = new Discord.Client({disableEveryone: true});
 
-bot.on("message", async message => {
+client.on('ready', () => {
+    
+        console.log(`Logged in as ${client.user.username}, ready to get this bread.`);
+        client.user.setActivity('Testing stuff');
+    
+    });
+    
+client.on("message", async message => {
 
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
@@ -17,9 +19,28 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
-  }
+  if (cmd === `${prefix}start`){
+for (var i = 0; i < 10000000000000000; i++) {
+message.channel.send('pls pm')
+await sleep(1000);
+message.channel.send('d');
+await sleep(2000)
+message.channel.send('pls beg');
+await sleep(2000)
+message.channel.send('pls search')
+await sleep(2000)
+message.channel.send('dresser');
+await sleep(2000)
+message.channel.send('pls use candy');
+await sleep(29000)
+message.channel.send('pls search')
+await sleep(2000)
+message.channel.send('dresser');
+await sleep(10000)
+message.channel.send('pls beg');
+await sleep(20000)
+}
+}
 });
-
-bot.login(process.env.token);
+ 
+client.login(process.env.token)
