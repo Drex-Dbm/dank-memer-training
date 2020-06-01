ed388712d2693afa7c3301c61f340a8326bedea8
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-01 15:08:39 +1200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ client.on("message", async message => {
   if (cmd === `${prefix}start`){
 for (var i = 0; i < 10000000000000000; i++) {
 message.channel.send('pls pm')
-await sleep(1000);
 message.channel.send('d');
 await sleep(2000)
 message.channel.send('pls beg');
