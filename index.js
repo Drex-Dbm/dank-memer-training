52a56e3fefc2f819b074c3e99dc495204c1fccc4
astreminix <66201263+astreminix@users.noreply.github.com>
2020-06-08 20:30:04 +1200

    Update index.js

--- index.js
+++ index.js
@@ -31,10 +31,40 @@ var channel = server.channels.get("716227489860681743")
         await sleep(31000)
         channel.send('pls use candy')
         await sleep(1000)
+    
+
+client.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = '-';
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+  if (cmd === `${prefix}start`){
+for (var i = 0; i < 10000000000000000; i++) {
+channel.send('pls search')
+await sleep(1000)
+message.channel.send('dresser');
+await sleep(5000)
+message.channel.send('pls beg')
+await sleep(25100)
+        message.channel.send('pls search');
+        await sleep(1000)
+        message.channel.send('dresser');
+        await sleep(5000)
+        message.channel.send('pls pm')
+        await sleep(1000)
+        message.channel.send('d');
+        await sleep(2000)
+        message.channel.send('pls beg')
+        await sleep(31000)
+        message.channel.send('pls use candy')
+        await sleep(1000)
 }
- 
-    
-    });
-    
+}
+});
 
 client.login(process.env.token)
